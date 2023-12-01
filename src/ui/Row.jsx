f320f63b0ca7e10603860e630ba5f33703f9cd66
shshamim63 <shshamim63@gmail.com>
2023-12-01 16:56:45 +0600

    Added basic reuseable ui across pages

---
+++ src/ui/Row.jsx
@@ -0,0 +1,13 @@
+import styled, { css } from "styled-components";
+
+const Row = styled.div`
+  display: flex;
+  ${(props) =>
+    props.type === "horizontal" &&
+    css`
+      justify-content: space-around;
+      align-items: center;
+    `}
+`;
+
+export default Row;
