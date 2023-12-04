45cc00c381b5a82dd1e800736f3901b59f8a2c24
shshamim63 <shshamim63@gmail.com>
2023-12-04 23:24:13 +0600

    Configured supabase to the codebase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://xmidkkfohnwjfurpihpn.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
