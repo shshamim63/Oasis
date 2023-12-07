e402b7eb66da086f09f89cf9ac8c4af415839eeb
shshamim63 <shshamim63@gmail.com>
2023-12-07 17:24:21 +0600

    Introduced compound component for completing the creation of the modal

---
+++ src/features/cabins/AddCabin.jsx
@@ -0,0 +1,35 @@
+import Button from "../../ui/Button";
+import Modal from "../../ui/Modal";
+import CreateCabinForm from "./CreateCabinForm";
+
+function AddCabin() {
+  return (
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add new Cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+    </Modal>
+  );
+}
+
+// function AddCabin() {
+//   const [isOPenModal, setIsOpenModal] = useState(false);
+
+//   return (
+//     <div>
+//       <Button onClick={() => setIsOpenModal((show) => !show)}>
+//         Add new cabin
+//       </Button>
+//       {isOPenModal && (
+//         <Modal onClose={() => setIsOpenModal(false)}>
+//           <CreateCabinForm onCloseModal={() => setIsOpenModal(false)}/>
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
+
+export default AddCabin;
