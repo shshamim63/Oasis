3183cd1c2b6bd600c88745567817217deaccdd40
shshamim63 <shshamim63@gmail.com>
2023-12-26 23:14:07 +0600

    Added login functionality

---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,42 @@
+import PropTypes from "prop-types";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+import styled from "styled-components";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+  const { user, isLoading } = useUser();
+
+  useEffect(() => {
+    if (!user && user?.role !== "authenticated") navigate("/login");
+  }, [user, navigate]);
+
+  if (isLoading) {
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+  }
+
+  return children;
+}
+
+ProtectedRoute.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+};
+
+export default ProtectedRoute;
