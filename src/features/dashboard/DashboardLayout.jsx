b3492b11021dd1d133db04c0a5a6a9016deffffb
shshamim63 <shshamim63@gmail.com>
2024-01-02 22:24:45 +0600

    Added Dashboard layout

---
+++ src/features/dashboard/DashboardLayout.jsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
+import Stats from "./Stats";
+import { useCabins } from "../cabins/useCabins";
+import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity";
+
+const StyledDashboardLayout = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: auto 34rem auto;
+`;
+
+function DashboardLayout() {
+  const { bookings, isLoading: isBookingsLoading } = useRecentBookings();
+  const {
+    isLoading: isStaysLoading,
+    confirmedStays,
+    numDays,
+  } = useRecentStays();
+  const { cabins, isLoading: isCabinLoading } = useCabins();
+
+  if (isBookingsLoading || isStaysLoading || isCabinLoading) return <Spinner />;
+  
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCounts={cabins.length}
+      />
+      <TodayActivity />
+      <DurationChart confirmedStays={confirmedStays} />
+      <SalesChart bookings={bookings} numDays={numDays} />
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
